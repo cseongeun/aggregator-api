e023bb7fc91cd11588cbaf9762f8255ad88167d3
eun <eun@hexlant.com>
2022-01-13 15:30:22 +0900

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AddressModule } from '../address/address.module';
 import { MysqlConfigService } from './mysql/mysql-config.service';
 
 @Module({
-  imports: [
-    AddressModule,
-    TypeOrmModule.forRootAsync({ useClass: MysqlConfigService }),
-  ],
+  imports: [TypeOrmModule.forRootAsync({ useClass: MysqlConfigService })],
 })
 export class AppModule {}
