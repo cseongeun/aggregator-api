cfe9f819fadbdef38574e71db3b6d57eed81ddae
eun <eun@hexlant.com>
2022-01-21 18:15:46 +0900

    ASDFASDF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MysqlConfigService } from './mysql/mysql-config.service';
+import { MysqlConfigService } from './mysql/mysql.config';
 
 @Module({
   imports: [TypeOrmModule.forRootAsync({ useClass: MysqlConfigService })],
